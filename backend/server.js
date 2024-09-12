9df7c96a40efa8ee09c6f48144c89ff888513d21
Romtouf <romtouf@laposte.net>
2024-09-12 15:28:16 +0200

    v5

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,7 @@ const io = new Server(server, {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
   })
