d12e1115fd0dcf3792f2ff8759d0fc85fa186314
Romtouf <romtouf@laposte.net>
2024-09-12 14:36:12 +0200

    Backend modifié pour Hostinger

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,21 @@ const authRoutes = require("./routes/auth");
 const Message = require("./models/Message");
 const dotenv = require("dotenv");
 const cron = require("node-cron");
+const fs = require("fs");
+const https = require("https");
 
 dotenv.config();
 
 const app = express();
-const server = http.createServer(app);
+const options = {
+  key: fs.readFileSync("path/to/your/private.key"),
+  cert: fs.readFileSync("path/to/your/certificate.crt"),
+};
+
+const server = https.createServer(options, app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://localhost:3000",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -90,7 +97,7 @@ cron.schedule("*/15 * * * *", async () => {
   console.log("Messages older than 15 minutes deleted");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
