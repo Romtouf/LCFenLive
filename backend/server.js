46fa4bbe9699433c5c58c342fc160a120d52aad4
Romtouf <romtouf@laposte.net>
2024-09-12 14:58:07 +0200

    v4 Hostinger

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
+const fs = require("fs");
+const https = require("https");
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
-const http = require("http");
 const { Server } = require("socket.io");
 const authRoutes = require("./routes/auth");
 const Message = require("./models/Message");
@@ -16,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://localhost:3000",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -35,8 +36,13 @@ mongoose
   });
 
 app.use(morgan("dev"));
-app.use(helmet());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  helmet({
+    crossOriginOpenerPolicy: { policy: "same-origin" },
+    originAgentCluster: true,
+  })
+);
+app.use(cors({ origin: "https://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
@@ -94,7 +100,7 @@ cron.schedule("*/15 * * * *", async () => {
   console.log("Messages older than 15 minutes deleted");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
